1caf7a069f88deb1de5b9fa318d1872f7609ce7b
Corebolt <mgfeisthauer@hotmail.com>
2023-02-21 15:22:40 -0500

    working on part 8

---
+++ src/WordFrequency.java
@@ -0,0 +1,48 @@
+public class WordFrequency {
+
+    int index;
+    Integer frequency;
+    String word;
+    static Integer nextIndex = 1000;
+
+    public WordFrequency(String word, Integer frequency){
+        this.word = word;
+        this.frequency = frequency;
+        this.index = nextIndex++;
+    }
+
+    public String getWord()
+    {
+        return word;
+    }
+
+    public void setWord(String newWord)
+    {
+        this.word = newWord;
+    }
+
+    @Override
+    public String toString() {
+        return "Word = " + this.word + " Frequency = " + frequency;
+    }
+
+    @Override
+    public int compareTo(WordFrequency otherWordFrequency)
+    {
+        int compareValue = 0;
+        if(this.frequency > otherWordFrequency.frequency)
+        {
+            compareValue = 1;
+        }
+        if(this.frequency < otherWordFrequency.frequency)
+        {
+            compareValue = -1;
+        }
+        if(this.frequency == otherWordFrequency.frequency)
+        {
+            compareValue = 0;
+        }
+        return compareValue;
+    }
+    
+}
